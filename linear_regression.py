2639f0e59f95dbc3408fc1c86d9d211c532fc216
anushikha700 <anushikha700@gmail.com>
2019-05-16 17:15:42 +0530

    Update linear_regression.py

--- linear_regression.py
+++ linear_regression.py
@@ -9,17 +9,17 @@ print(df)
 reg=linear_model.LinearRegression()
 reg.fit(df[['area']],df.price)
 
-plt.xlabel("Area(sq ft)")
-plt.ylabel("Price ($)")
+plt.xlabel("Area(sq ft)") # label x-axis in graph
+plt.ylabel("Price ($)")# label y-axis in graph
 plt.title("Area-Price")
 
-plt.scatter(df.area,df.price,color='red',marker='+')
+plt.scatter(df.area,df.price,color='red',marker='*')
 plt.plot(df.area,reg.predict(df[['area']]),color='blue')
 plt.show()
 
-print("Prediction : ",reg.predict([[500]]))
-print("Prediction : ",reg.predict([[530],[700]]))
-print("Coefficien : ",reg.coef_)
-print("Intercept : ",reg.intercept_)
+print("Prediction : ",reg.predict([[500]]))  # Prediction :  [16160.10638298]
+print("Prediction : ",reg.predict([[530],[700]]))] #Prediction :  [17023.40425532 21915.42553191]
+print("Coefficient : ",reg.coef_) # Coefficient :  [28.77659574]
+print("Intercept : ",reg.intercept_) #Intercept :  1771.8085106382969
 
 
